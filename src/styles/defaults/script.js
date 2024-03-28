4ce97b67e344850629563ffb5c9b7c57b3882b2f
sofia <126134345+sooffia@users.noreply.github.com>
2024-03-29 05:43:26 +1100

    Add final edits to about page

--- src/styles/defaults/script.js
+++ src/styles/defaults/script.js
@@ -1,5 +1,5 @@
 const navbar = document.getElementById("navbar");
-const menu = document.getElementById("icon-list");
+const menu = document.getElementById("menu");
 
 menu.addEventListener("click", () => {
   navbar.classList.toggle("active");
